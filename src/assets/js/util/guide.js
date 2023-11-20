b0421b42d9a25cf8152a9e44ab5cfa67eed2d41f
Kalana Dananjaya <kalanadananjaya855@gmail.com>
2023-11-20 22:09:59 +0530

    package and service done

--- src/assets/js/util/guide.js
+++ src/assets/js/util/guide.js
@@ -1,33 +1,156 @@
+getAll();
+
+var isMatchGuideId = false;
+var isMatchGuideName = false;
+var isMatchGuideAddress = false;
+var isMatchGuideTelNo = false;
+var isMatchGuideExperience = false;
+var isMatchGuideManDayValue = false;
+
+
+
+var regExGuideID = /^(G00-)[0-9]{3,4}$/
+
+$('#guideId').keyup(function () {
+    var val = $('#guideId').val();
+    if (regExGuideID.test(val)) {
+        $('#guideId').css('border', '2px solid green')
+        $('#idError').text("");
+        isMatchGuideId = true;
+    } else {
+        $('#guideId').css("border", "2px solid red");
+        $('#idError').text("wrong format : G00-001");
+        isMatchGuideId = false;
+    }
+});
+
+
+var regExGuideName = /^[a-zA-Z]{3,15}$/
+
+$("#guideName").keyup(function () {
+    var name = $("#guideName").val();
+    if (regExGuideName.test(name)) {
+        $("#guideName").css("border", "2px solid green");
+        $("#nameError").text("");
+        isMatchGuideName = true;
+    } else {
+        $("#guideName").css("border", "2px solid red");
+        $("#nameError").text("wrong format : A-Z or a-z");
+        isMatchGuideName = false;
+    }
+});
+
+
+var regExAddress = /^[a-zA-Z]{3,25}$/;
+$('#address').keyup(function (){
+    let address = $('#address').val();
+    if (regExAddress.test(address)){
+        $('#address').css("border", "2px solid green");
+        $('#addressError').text("");
+        isMatchGuideAddress=true;
+    }else {
+        $('#address').css("border", "2px solid red");
+        $('#addressError').text("wrong format : a-z or A-Z");
+        isMatchGuideAddress=false;
+    }
+
+});
+
+
+var regExGuideNumber = /^(?:\+94|0)[1-9]\d{8}$/;
+
+$('#number').keyup(function (){
+    let num = $('#number').val();
+    if (regExGuideNumber.test(num)){
+        $('#number').css("border", "2px solid green");
+        $('#telNoError').text("");
+        isMatchGuideTelNo=true;
+    }else {
+        $('#number').css("border", "2px solid red");
+        $('#telNoError').text("wrong format : +94769151600 | 0769151600");
+        isMatchGuideTelNo=false;
+    }
+
+});
+
+
+
+var regExGuideExperience = /^[A-z ]{3,20}$/;
+$('#experience').keyup(function (){
+    let exp = $('#experience').val();
+    if (regExGuideExperience.test(exp)){
+        $('#experience').css("border", "2px solid green");
+        $('#ExpError').text("");
+        isMatchGuideExperience=true;
+    }else {
+        $('#experience').css("border", "2px solid red");
+        $('#ExpError').text("wrong format : a-z or A-Z");
+        isMatchGuideExperience=false;
+    }
+
+});
+
+
+var regExManDayValue = /^[0-9.]{2,10}$/;
+$('#manDayValue').keyup(function (){
+    let value = $('#manDayValue').val();
+    if (regExManDayValue.test(value)){
+        $('#manDayValue').css("border", "2px solid green");
+        $('#manDayValueError').text("");
+        isMatchGuideManDayValue=true;
+    }else {
+        $('#manDayValue').css("border", "2px solid red");
+        $('#manDayValueError').text("wrong format : 100.00 or 100");
+        isMatchGuideManDayValue=false;
+    }
+
+});
+
+
+
+
+
 
 /* Save */
 
 function save(){
-    let guideId = $('#guideId').val();
-    let guideName = $('#guideName').val();
-    let address = $('#address').val();
-    let gender = $('#gender').val();
-    let number = $('#number').val();
-    let experience = $('#experience').val();
-    let manDayValue = $('#manDayValue').val();
-    let guideImage = $('#guideImage').val();
-    let nicImage = $('#nicImage').val();
+
+    if (!isMatchGuideId) {
+    } else if (!isMatchGuideName){
+    } else if (!isMatchGuideAddress) {
+    } else if (!isMatchGuideTelNo) {
+    } else if (!isMatchGuideExperience){
+    } else if (!isMatchGuideManDayValue){
+    }else {
+
+        let guideId = $('#guideId').val();
+        let guideName = $('#guideName').val();
+        let address = $('#address').val();
+        let gender = $('#gender').val();
+        let number = $('#number').val();
+        let experience = $('#experience').val();
+        let manDayValue = $('#manDayValue').val();
+        let guideImage = $('#guideImage')[0].files[0];
+        let nicImage = $('#nicImage')[0].files[0];
 
 
-    $.ajax({
-        type: "POST",
-        url: "",
-        data: {guideId:guideId, guideName:guideName, address:address, gender:gender, number:number,
-            experience:experience, manDayValue:manDayValue , guideImage:guideImage ,  nicImage
-        },
-        success: (response) => {
-            console.log(response)
-            getAll()
-        },
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:8080/guideService/api/v1/guide",
+            data: {guideId:guideId, guideName:guideName, address:address, gender:gender, number:number,
+                experience:experience, manDayValue:manDayValue , guideImage:guideImage ,  nicImage
+            },
+            success: (response) => {
+                console.log(response)
+                getAll()
+            },
 
-        error: function (error) {
-            console.log(error)
-        }
-    })
+            error: function (error) {
+                console.log(error)
+            }
+        })
+
+    }
 }
 
 
@@ -35,31 +158,41 @@ function save(){
 
 function update() {
 
-    let guideId = $('#guideId').val();
-    let guideName = $('#guideName').val();
-    let address = $('#address').val();
-    let gender = $('#gender').val();
-    let number = $('#number').val();
-    let experience = $('#experience').val();
-    let manDayValue = $('#manDayValue').val();
-    let guideImage = $('#guideImage').val();
-    let nicImage = $('#nicImage').val();
+    if (!isMatchGuideId) {
+    } else if (!isMatchGuideName){
+    } else if (!isMatchGuideAddress) {
+    } else if (!isMatchGuideTelNo) {
+    } else if (!isMatchGuideExperience){
+    } else if (!isMatchGuideManDayValue){
+    }else {
 
-    $.ajax({
-        type: "PUT",
-        url: "",
-        data: {guideId:guideId, guideName:guideName, address:address, gender:gender, number:number,
-            experience:experience, manDayValue:manDayValue , guideImage:guideImage ,  nicImage
-        },
-        success: (response) => {
-            console.log(response)
-            getAll()
-        },
+        let guideId = $('#guideId').val();
+        let guideName = $('#guideName').val();
+        let address = $('#address').val();
+        let gender = $('#gender').val();
+        let number = $('#number').val();
+        let experience = $('#experience').val();
+        let manDayValue = $('#manDayValue').val();
+        let guideImage = $('#guideImage')[0].files[0];
+        let nicImage = $('#nicImage')[0].files[0];
 
-        error: function (error) {
-            console.log(error)
-        }
-    })
+        $.ajax({
+            type: "PUT",
+            url: "http://localhost:8080/guideService/api/v1/guide",
+            data: {guideId:guideId, guideName:guideName, address:address, gender:gender, number:number,
+                experience:experience, manDayValue:manDayValue , guideImage:guideImage ,  nicImage
+            },
+            success: (response) => {
+                console.log(response)
+                getAll()
+            },
+
+            error: function (error) {
+                console.log(error)
+            }
+        })
+    }
+
 }
 
 
@@ -69,7 +202,7 @@ function search() {
     let guideId = $('#guideId').val();
     $.ajax({
         type: "GET",
-        url: "" + guideId,
+        url: "http://localhost:8080/guideService/api/v1/guide" + guideId,
         async: true,
         success: (response) => {
             $('#guideId').val(response.guideId);
@@ -79,8 +212,6 @@ function search() {
             $('#number').val(response.number);
             $('#experience').val(response.experience);
             $('#manDayValue').val(response.manDayValue);
-            $('#guideImage').val(response.guideImage);
-            $('#nicImage').val(response.nicImage);
         },
 
         error: function (error) {
@@ -92,8 +223,6 @@ function search() {
             $('#number').val(" ");
             $('#experience').val(" ");
             $('#manDayValue').val(" ");
-            $('#guideImage').val(" ");
-            $('#nicImage').val(" ");
             alert("Guide Not Found..!");
         }
     })
@@ -106,7 +235,7 @@ function Delete() {
     let guideId = $('#guideId').val();
     $.ajax({
         type: "DELETE",
-        url: "" + guideId,
+        url: "http://localhost:8080/guideService/api/v1/guide" + guideId,
         success: (response) => {
             alert("Delete Success..!")
             getAll()
@@ -125,12 +254,12 @@ function getAll(){
     $('#tBody').empty();
     $.ajax({
         type: "Get",
-        url: "",
+        url: "http://localhost:8080/guideService/api/v1/guide",
         success: (response) => {
             response?.map(
                 (data) => {
                     let row = `<tr><td>${data.guideId}</td><td>${data.guideName}</td><td>${data.address}</td><td>${data.gender}</td><td>${data.number}</td><td>${data.experience}</td>
-                                <td>${data.manDayValue}</td><td>${data.guideImage}</td><td>${data.nicImage}</td></tr>`;
+                                <td>${data.manDayValue}</td></tr>`;
                     $('#tBody').append(row);
                 }
             )
@@ -140,4 +269,99 @@ function getAll(){
             alert("Guide Not Found..")
         }
     })
+    loadTextFieldValues()
 };
+
+
+function loadTextFieldValues(){
+    $("#tBody>tr").on("click",function (){
+        let guide_id = $(this).children().eq(0).text();
+        let guide_Name = $(this).children().eq(1).text();
+        let guide_Address = $(this).children().eq(2).text();
+        let guide_Gender = $(this).children().eq(3).text();
+        let guide_Number = $(this).children().eq(4).text();
+        let guide_Experience = $(this).children().eq(5).text();
+        let guide_Day_Value = $(this).children().eq(6).text();
+
+
+        $("#guideId").val(guide_id);
+        $("#guideName").val(guide_Name);
+        $("#address").val(guide_Address);
+        $("#gender").val(guide_Gender);
+        $("#number").val(guide_Number);
+        $("#experience").val(guide_Experience);
+        $("#manDayValue").val(guide_Day_Value);
+    });
+
+}
+
+
+
+//Auto Generate id
+function autoGenerateId() {
+    $("#guideId").val("G-001");
+    $.ajax({
+        url:"",
+        method:"GET",
+        contentType: "application/json",
+        dataType: "json",
+        success: function (resp) {
+            let id = resp.value;
+            let tempid = parseInt(id.split("-")[1]);
+            tempid = tempid + 1;
+            if (tempid <= 9) {
+                $("#guideId").val("G-00" + tempid);
+            } else if (tempid <= 99) {
+                $("#guideId").val("G-0" + tempid);
+            } else {
+                $("#guideId").val("G-" + tempid);
+            }
+        },
+
+        error:function (error){
+            console.log(error);
+        }
+    })
+}
+
+
+
+//----------------------Load Guide Details-----------------------------------------
+
+$("#guideId").click(function (){
+    var searchGuide=$("#guideId").val();
+    $.ajax({
+        url:"http://localhost:8989/nextTravelPVTLtdGuideService/api/v1/guide/searchGuide/?guide_id="+searchGuide,
+        method:"GET",
+        contentType:"application/json",
+        dataType:"json",
+        success:function (res){
+            $("#guideName").val(res.guideName);
+            $("#number").val(res.number);
+            $("#manDayValue").val(res.manDayValue);
+        },
+        error:function (error){
+            console.log(error);
+        }
+    })
+});
+
+//------------------------Select Guide or not--------------------------------
+
+/*$("#").click(function (){
+    let selectedOption=$(this).val();
+
+    if (selectedOption === "yes"){
+        $.ajax({
+            url:bookingBaseUrl+"guide/yesGuide",
+            method:"GET",
+            dataType:"json",
+            success:function (res){
+                $("#guide_id").val(res.guideid);
+            },
+            error:function (error){
+                console.log(error);
+            }
+        });
+    }
+})*/
--- src/assets/js/util/vehicle.js
+++ src/assets/js/util/vehicle.js
@@ -1,3 +1,10 @@
+// getAll();
+
+
+
+
+
+
 
 function save(){
     let registrationNumber = $('#registrationNumber').val();
@@ -6,21 +13,21 @@ function save(){
     let fuelType = $('#fuelType').val();
     let isHybrid = $('#isHybrid').val();
     let fuelUsage = $('#fuelUsage').val();
-    let frontView = $('#frontView').val();
-    let rearView = $('#rearView').val();
-    let sideView = $('#sideView').val();
-    let otherSideView = $('#otherSideView').val();
+    let frontView = $('#frontView')[0].files[0];
+    let rearView = $('#rearView')[0].files[0];
+    let sideView = $('#sideView')[0].files[0];
+    let otherSideView = $('#otherSideView')[0].files[0];
     let seatCapacity = $('#seatCapacity').val();
     let vehicleType = $('#vehicleType').val();
     let transmission = $('#transmission').val();
     let driverName = $('#driverName').val();
     let driverNumber = $('#driverNumber').val();
-    let licenseFront = $('#licenseFront').val();
-    let licenseRear = $('#licenseRear').val();
+    let licenseFront = $('#licenseFront')[0].files[0];
+    let licenseRear = $('#licenseRear')[0].files[0];
 
     $.ajax({
         type:"POST",
-        url:"",
+        url:"http://localhost:8080/vehicleService/api/v1/vehicle",
         data:{registrationNumber:registrationNumber, vehicleBrand:vehicleBrand, category:category, fuelType:fuelType,
             isHybrid:isHybrid, fuelUsage:fuelUsage, frontView:frontView, rearView:rearView, sideView:sideView,
             otherSideView:otherSideView, seatCapacity:seatCapacity, vehicleType:vehicleType, transmission:transmission,
@@ -45,21 +52,21 @@ function update(){
     let fuelType = $('#fuelType').val();
     let isHybrid = $('#isHybrid').val();
     let fuelUsage = $('#fuelUsage').val();
-    let frontView = $('#frontView').val();
-    let rearView = $('#rearView').val();
-    let sideView = $('#sideView').val();
-    let otherSideView = $('#otherSideView').val();
+    let frontView = $('#frontView')[0].files[0];
+    let rearView = $('#rearView')[0].files[0];
+    let sideView = $('#sideView')[0].files[0];
+    let otherSideView = $('#otherSideView')[0].files[0];
     let seatCapacity = $('#seatCapacity').val();
     let vehicleType = $('#vehicleType').val();
     let transmission = $('#transmission').val();
     let driverName = $('#driverName').val();
     let driverNumber = $('#driverNumber').val();
-    let licenseFront = $('#licenseFront').val();
-    let licenseRear = $('#licenseRear').val();
+    let licenseFront = $('#licenseFront')[0].files[0];
+    let licenseRear = $('#licenseRear')[0].files[0];
 
     $.ajax({
         type:"PUT",
-        url:"",
+        url:"http://localhost:8080/vehicleService/api/v1/vehicle",
         data:{registrationNumber:registrationNumber, vehicleBrand:vehicleBrand, category:category, fuelType:fuelType,
             isHybrid:isHybrid, fuelUsage:fuelUsage, frontView:frontView, rearView:rearView, sideView:sideView,
             otherSideView:otherSideView, seatCapacity:seatCapacity, vehicleType:vehicleType, transmission:transmission,
@@ -81,7 +88,7 @@ function Delete() {
     let registrationNumber = $('#registrationNumber').val();
     $.ajax({
         type: "DELETE",
-        url: "" + registrationNumber,
+        url: "http://localhost:8080/vehicleService/api/v1/vehicle" + registrationNumber,
         success: (response) => {
             alert("Delete Success..!")
             getAll()
@@ -96,19 +103,59 @@ function Delete() {
 
 
 
+function search() {
+    let ID = $('#registrationNumber').val();
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/vehicleService/api/v1/vehicle" + ID,
+        success: (response) => {
+            $('#registrationNumber').val(response.registrationNumber);
+            $('#vehicleBrand').val(response.vehicleBrand);
+            $('#category').val(response.category);
+            $('#fuelType').val(response.fuelType);
+            $('#isHybrid').val(response.isHybrid);
+            $('#fuelUsage').val(response.fuelUsage);
+            $('#seatCapacity').val(response.seatCapacity);
+            $('#vehicleType').val(response.vehicleType);
+            $('#transmission').val(response.transmission);
+            $('#driverName').val(response.driverName);
+            $('#driverNumber').val(response.driverNumber);
+        },
+
+        error: function (error) {
+            console.log(error)
+            $('#registrationNumber').val("");
+            $('#vehicleBrand').val("");
+            $('#category').val("");
+            $('#fuelType').val("");
+            $('#isHybrid').val("");
+            $('#fuelUsage').val("");
+            $('#seatCapacity').val("");
+            $('#vehicleType').val("");
+            $('#transmission').val("");
+            $('#driverName').val("");
+            $('#driverNumber').val("");
+            alert("Vehicle Not Found..!");
+        }
+    })
+}
+
+
+
+
+
 
 function getAll() {
     $('#tBody').empty();
     $.ajax({
         type: "Get",
-        url: "",
+        url: "http://localhost:8080/vehicleService/api/v1/vehicle",
         success: (response) => {
             response?.map(
                 (data) => {
                     let row = `<tr><td>${data.registrationNumber}</td><td>${data.vehicleBrand}</td><td>${data.category}</td><td>${data.fuelType}</td><td>${data.isHybrid}</td><td>${data.fuelUsage}</td>
-                                        <td>${data.frontView}</td><td>${data.rearView}</td><td>${data.sideView}</td><td>${data.otherSideView}</td>
                                         <td>${data.seatCapacity}</td><td>${data.vehicleType}</td><td>${data.transmission}</td><td>${data.driverName}</td>
-                                        <td>${data.driverNumber}</td><td>${data.licenseFront}</td><td>${data.licenseRear}</td></tr>`;
+                                        <td>${data.driverNumber}</td></tr>`;
                     $('#tBody').append(row);
                 }
             )
@@ -118,4 +165,39 @@ function getAll() {
             alert("Vehicle Not Found..")
         }
     })
+    loadTextFieldValues();
+}
+
+
+
+
+function loadTextFieldValues() {
+    $("#tBody>tr").on("click", function () {
+        let Vehicle_id = $(this).children().eq(0).text();
+        let Vehicle_Brand = $(this).children().eq(1).text();
+        let Vehicle_category = $(this).children().eq(2).text();
+        let Vehicle_fuelType = $(this).children().eq(3).text();
+        let Vehicle_isHybrid = $(this).children().eq(4).text();
+        let Vehicle_fuelUsage = $(this).children().eq(5).text();
+        let Vehicle_seatCapacity = $(this).children().eq(6).text();
+        let Vehicle_Type = $(this).children().eq(7).text();
+        let Vehicle_transmission = $(this).children().eq(8).text();
+        let Vehicle_driverName = $(this).children().eq(9).text();
+        let Vehicle_driverNumber = $(this).children().eq(10).text();
+
+
+
+        $('#registrationNumber').val(Vehicle_id);
+        $('#vehicleBrand').val(Vehicle_Brand);
+        $('#category').val(Vehicle_category);
+        $('#fuelType').val(Vehicle_fuelType);
+        $('#isHybrid').val(Vehicle_isHybrid);
+        $('#fuelUsage').val(Vehicle_fuelUsage);
+        $('#seatCapacity').val(Vehicle_seatCapacity);
+        $('#vehicleType').val(Vehicle_Type);
+        $('#transmission').val(Vehicle_transmission);
+        $('#driverName').val(Vehicle_driverName);
+        $('#driverNumber').val(Vehicle_driverNumber);
+
+    });
 }
